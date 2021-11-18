232a8e013349cf332deb108dc58346325cdd6058
Luis <luisgallego778@gmail.com>
2021-11-18 18:30:21 +0000

    Added support for merhod GET /contacts

--- index.js
+++ index.js
@@ -23,7 +23,14 @@ app.get('/', (req, res) => {
 
 app.get(BASE_API_PATH + "/contacts", (req,res) =>{
     console.log(Date() + " - GET /contacts");
-    res.send([])
+    db.find({}, (err, contacts) => {
+      if(err){
+        console.log(Date() + " - " + err);
+      }
+      else{
+        res.send(contacts);
+      }
+    });
 });
 
 app.post(BASE_API_PATH + "/contacts", (req,res) =>{
