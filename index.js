c78c720d72ddb6679c71ebbf79e07c84e7faf842
Luis <luisgallego778@gmail.com>
2021-12-02 12:37:16 +0000

    Backend modified to use MongoDB

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const app = require('./server.js'); 
+const dbConnect = require('./db');
 
 var port = (process.env.PORT || 3000); 
 
 console.log("Starting API server at "+port); 
 
-app.listen(port); 
+dbConnect().then(
+    () => {
+        app.listen(port);
+        console.log("Server ready!"); 
+    },
+    err => {
+        console.log("Connection error: " + err);
+    }
+)
 
-console.log("Server ready!"); 
+
